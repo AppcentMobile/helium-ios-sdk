6723cbde005f88aaea41a5e9721feb2d60a26b06
Burak Colak <burak@bcolak.com>
2022-10-14 00:55:10 +0300

    🎷 [FEATURE] Console routes initiated.

---
+++ Sources/Helium-iOS-SDK/Routes/Console/ConsoleRoutes.swift
@@ -0,0 +1,16 @@
+//
+//  ConsoleRoutes.swift
+//  
+//
+//  Created by Burak Colak on 14.10.2022.
+//
+
+import UIKit
+
+struct ConsoleRoutes {
+
+    static let transactionForHash = "api/v1/organization"
+    static let devicesList = "api/v1/devices"
+    static let devicesByAppEuiAppKeyDevEui = "api/v1/devices"
+    static let deviceByUUID
+}
