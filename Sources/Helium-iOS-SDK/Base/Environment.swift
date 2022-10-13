2b71941edc5b8f5351be834685873331d7d22c3b
Burak Colak <burak@bcolak.com>
2022-10-13 23:58:29 +0300

    🎷 [FEATURE] Environment added.

---
+++ Sources/Helium-iOS-SDK/Base/Environment.swift
@@ -0,0 +1,15 @@
+//
+//  Environment.swift
+//  
+//
+//  Created by Burak Colak on 13.10.2022.
+//
+
+import UIKit
+
+enum Environment: String {
+
+    case BETA = "https://api.helium.wtf"
+    case SCALABLE = "https://api.helium.io"
+
+}
