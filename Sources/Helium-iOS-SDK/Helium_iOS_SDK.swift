3b7717e45398c60d66d3a3eba626df704135ecc2
Burak Colak <burak@bcolak.com>
2022-10-14 16:47:07 +0300

    🎷 [FEATURE] Config updated.

--- Sources/Helium-iOS-SDK/Helium_iOS_SDK.swift
+++ Sources/Helium-iOS-SDK/Helium_iOS_SDK.swift
@@ -5,12 +5,12 @@ Helium iOS SDK
 
 public struct Helium_iOS_SDK {
 
-    static public func initialize(config: HeliumConfig) {
+    static public func initialize(apiKey: String, console: HeliumEnvironment.CONSOLE, blockchain: HeliumEnvironment.BLOCKCHAIN, logEnabled: Bool) {
 
-        Constants.API_KEY = config.apiKey
-        Constants.CONSOLE_ENVIRONMENT = config.console
-        Constants.BLOCKHAIN_ENVIRONMENT = config.blockchain
-        Constants.LOG_ENABLED = config.logEnabled
+        Constants.API_KEY = apiKey
+        Constants.CONSOLE_ENVIRONMENT = console
+        Constants.BLOCKHAIN_ENVIRONMENT = blockchain
+        Constants.LOG_ENABLED = logEnabled
 
     }
 
