161a3212824811ae49a88195a863b841e81c0f0b
Burak Colak <burak@bcolak.com>
2022-10-14 16:53:26 +0300

    🎷 [FEATURE] Initialize comments added.

--- Sources/Helium-iOS-SDK/Helium_iOS_SDK.swift
+++ Sources/Helium-iOS-SDK/Helium_iOS_SDK.swift
@@ -5,12 +5,26 @@ Helium iOS SDK
 
 public struct Helium_iOS_SDK {
 
-    static public func initialize(apiKey: String, console: HeliumEnvironment.CONSOLE, blockchain: HeliumEnvironment.BLOCKCHAIN, logEnabled: Bool) {
+    /**
+        Initializes the helium iOS SDK;
+     - Author:
+     Burak
+
+     - returns:
+     void
+
+     - parameters:
+        - apiKey: String (Required)
+        - console: Represents the console api base URL; HeliumEnvironment.CONSOLE (Optional), default: BASE
+        - blockchain: Represents the blockhain api base URL; HeliumEnvironment.BLOCKCHAIN (Optional), default: SCALABLE
+        - logEnabled: Manages network logs; Bool (Optional), default: false
+     */
+    static public func initialize(apiKey: String, console: HeliumEnvironment.CONSOLE? = .BASE, blockchain: HeliumEnvironment.BLOCKCHAIN? = .SCALABLE, logEnabled: Bool? = false) {
 
         Constants.API_KEY = apiKey
-        Constants.CONSOLE_ENVIRONMENT = console
-        Constants.BLOCKHAIN_ENVIRONMENT = blockchain
-        Constants.LOG_ENABLED = logEnabled
+        Constants.CONSOLE_ENVIRONMENT = console ?? .BASE
+        Constants.BLOCKHAIN_ENVIRONMENT = blockchain ?? .SCALABLE
+        Constants.LOG_ENABLED = logEnabled ?? false
 
     }
 
