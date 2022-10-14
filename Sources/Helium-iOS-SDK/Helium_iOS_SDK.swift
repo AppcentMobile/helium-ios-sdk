ea61a33e8d29a772116ec56216e6c6f43e3b06c1
Burak Colak <burak@bcolak.com>
2022-10-14 16:55:18 +0300

    🎷 [UPDATE] Comment updated.

--- Sources/Helium-iOS-SDK/Helium_iOS_SDK.swift
+++ Sources/Helium-iOS-SDK/Helium_iOS_SDK.swift
@@ -7,6 +7,7 @@ public struct Helium_iOS_SDK {
 
     /**
         Initializes the helium iOS SDK;
+
      - Author:
      Burak
 
@@ -19,6 +20,7 @@ public struct Helium_iOS_SDK {
         - blockchain: Represents the blockhain api base URL; HeliumEnvironment.BLOCKCHAIN (Optional), default: SCALABLE
         - logEnabled: Manages network logs; Bool (Optional), default: false
      */
+
     static public func initialize(apiKey: String, console: HeliumEnvironment.CONSOLE? = .BASE, blockchain: HeliumEnvironment.BLOCKCHAIN? = .SCALABLE, logEnabled: Bool? = false) {
 
         Constants.API_KEY = apiKey
