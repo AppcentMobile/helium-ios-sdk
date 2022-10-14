5fecd97f8c296c9d3624d37ce4f510ca10fecbd9
Burak Colak <burak@bcolak.com>
2022-10-14 16:59:20 +0300

    🎷 [UPDATE] Initialization markup cleanup.

--- Sources/Helium-iOS-SDK/Helium_iOS_SDK.swift
+++ Sources/Helium-iOS-SDK/Helium_iOS_SDK.swift
@@ -19,8 +19,13 @@ public struct Helium_iOS_SDK {
         - console: Represents the console api base URL; HeliumEnvironment.CONSOLE (Optional), default: BASE
         - blockchain: Represents the blockhain api base URL; HeliumEnvironment.BLOCKCHAIN (Optional), default: SCALABLE
         - logEnabled: Manages network logs; Bool (Optional), default: false
-     */
 
+     - Important:
+     This function must be called before calling Helium API
+
+     - Version:
+     0.1
+     */
     static public func initialize(apiKey: String, console: HeliumEnvironment.CONSOLE? = .BASE, blockchain: HeliumEnvironment.BLOCKCHAIN? = .SCALABLE, logEnabled: Bool? = false) {
 
         Constants.API_KEY = apiKey
