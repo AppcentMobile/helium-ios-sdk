f60348fb17b461335c5ac35379b6fa9421c68df7
Burak Colak <burak@bcolak.com>
2022-10-16 11:24:20 +0300

    🎷 [FEATURE] Base manager all messages moved to public variable, now all the log messages are configurable by the developer.

--- Sources/Helium-iOS-SDK/Manager/Base/BaseManager.swift
+++ Sources/Helium-iOS-SDK/Manager/Base/BaseManager.swift
@@ -9,40 +9,51 @@ import UIKit
 
 class BaseManager {
 
-    private final let errorMessage = "An error occurred."
+    public var errorMessage = "An error occurred."
+    public var responseInfoMessage = "RESPONSE:"
+    public var responseNullMessage = "Response null"
+    public var dataNullMessage = "Data null"
+    public var dataParseSuccessMessage = "Data parsed successfully"
+    public var dataParseErrorMessage = "Data parsing error : %@"
+    public var urlRequestErrorMessage = "URL Request Error"
+    public var httpURLMessage = "HTTP URL:"
+    public var httpAuthHeadersMessage = "HTTP AUTH HEADERS:"
+    public var httpHeadersMessage = "HTTP HEADERS:"
+    public var httpQueryItemsMessage = "HTTP QUERYITEMS:"
+    public var httpBodyMessage = "HTTP BODY:"
 
     private var session: URLSession {
         let configuration = URLSessionConfiguration.default
-        configuration.timeoutIntervalForRequest = 120
-        configuration.timeoutIntervalForResource = 120
+        configuration.timeoutIntervalForRequest = Constants.TIMEOUT
+        configuration.timeoutIntervalForResource = Constants.TIMEOUT
 
         return URLSession(configuration: configuration, delegate: nil, delegateQueue: OperationQueue.main)
     }
 
     func request<T: Decodable>(to endpoint: BaseEndpoint, completion: @escaping (BaseResult<T, Error>) -> ()) {
         guard let urlRequest = endpoint.urlRequest else {
-            BaseLogger.error("URL Request Error")
+            BaseLogger.error(urlRequestErrorMessage)
             return
         }
 
         if let data = endpoint.url {
-            BaseLogger.info("HTTP URL:\(data)")
+            BaseLogger.info(self.httpURLMessage + "\(data)")
         }
 
         if let authHeader = endpoint.authHeader, authHeader.count > 0 {
-            BaseLogger.info("HTTP AUTH HEADERS:\(authHeader)")
+            BaseLogger.info(self.httpAuthHeadersMessage + "\(authHeader)")
         }
 
         if let data = endpoint.headers {
-            BaseLogger.info("HTTP HEADERS:\(data)")
+            BaseLogger.info(self.httpHeadersMessage + "\(data)")
         }
 
         if let data = endpoint.queryItems {
-            BaseLogger.info("HTTP QUERYITEMS:\(data)")
+            BaseLogger.info(self.httpQueryItemsMessage + "\(data)")
         }
 
         if let data = urlRequest.httpBody {
-            BaseLogger.info("HTTP BODY:" + String(decoding: data, as: UTF8.self))
+            BaseLogger.info(self.httpBodyMessage + String(decoding: data, as: UTF8.self))
         }
 
         let dataTask = session.dataTask(with: urlRequest) { [weak self] data, response, error in
@@ -51,28 +62,29 @@ class BaseManager {
                 return
             }
             guard response != nil else {
-                completion(.failure(BaseNetworkError(message: self?.errorMessage, log: "Response null", endpoint: endpoint)))
+                completion(.failure(BaseNetworkError(message: self?.errorMessage, log: self?.responseNullMessage, endpoint: endpoint)))
                 return
             }
             guard let data = data else {
-                completion(.failure(BaseNetworkError(message: self?.errorMessage, log: "Data null", endpoint: endpoint)))
+                completion(.failure(BaseNetworkError(message: self?.errorMessage, log: self?.dataNullMessage, endpoint: endpoint)))
                 return
             }
 
             if error?.isConnectivityError ?? false {
-                completion(.failure(BaseNetworkError(message: self?.errorMessage, log: "Data null", endpoint: endpoint)))
+                completion(.failure(BaseNetworkError(message: self?.errorMessage, log: self?.dataNullMessage, endpoint: endpoint)))
                 return
             }
 
-            BaseLogger.info("RESPONSE:" + String(decoding: data, as: UTF8.self))
+            BaseLogger.info(self?.responseInfoMessage ?? "" + String(decoding: data, as: UTF8.self))
 
             do {
                 let responseObject = try JSONDecoder().decode(T.self, from: data)
+                BaseLogger.warning(self?.dataParseSuccessMessage)
                 completion(.success(responseObject))
             } catch let e {
-                BaseLogger.warning("Data error : \(e)")
-                BaseLogger.warning("Data parsing error : \(e.localizedDescription)")
-                completion(.failure(BaseNetworkError(message: "Bir hata oluştu", log: "JSON Decoder Error: \(e)", endpoint: endpoint)))
+                let errorMessage = String(format: self?.dataParseErrorMessage ?? "", e.localizedDescription)
+                BaseLogger.warning(errorMessage)
+                completion(.failure(BaseNetworkError(message: self?.errorMessage, log: errorMessage, endpoint: endpoint)))
             }
         }
         dataTask.resume()
