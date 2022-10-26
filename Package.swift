afce09e47fbade612e443cf1775fc8c9a049fafb
Burak Colak <burak@bcolak.com>
2022-10-26 16:38:01 +0300

    🎷 [UPDATE] SDK name updated.

--- Package.swift
+++ Package.swift
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Helium-iOS-SDK",
+            name: "HeliumSDK",
             dependencies: []),
         .testTarget(
-            name: "Helium-iOS-SDKTests",
-            dependencies: ["Helium-iOS-SDK"]),
+            name: "HeliumSDKTests",
+            dependencies: ["HeliumSDK"]),
     ]
 )
--- Tests/HeliumSDKTests/HeliumSDKTests.swift
+++ Tests/HeliumSDKTests/HeliumSDKTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import Helium_iOS_SDK
+@testable import HeliumSDK
 
-final class Helium_iOS_SDKTests: XCTestCase {
+final class HeliumSDKTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
