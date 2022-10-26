66ddfa331dab9a9d462b7644c8b144329ce276c4
Burak Colak <burak@bcolak.com>
2022-10-26 16:25:56 +0300

    🎷 [UPDATE] Package name updated.

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "Helium-iOS-SDK",
+    name: "HeliumSDK",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Helium-iOS-SDK",
+            name: "HeliumSDK",
             targets: ["Helium-iOS-SDK"]),
     ],
     dependencies: [
